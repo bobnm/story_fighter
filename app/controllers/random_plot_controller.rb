261a2b57e701ef22648e12fb4423d1adca6655a5
Bob <bobnmfrog@gmail.com>
2014-10-11 16:40:22 +0900

    del comment

--- app/controllers/random_plot_controller.rb
+++ app/controllers/random_plot_controller.rb
@@ -1,6 +1,4 @@
 class RandomPlotController < ApplicationController
-  # GET /plot_pieces
-  # GET /plot_pieces.json
   def index
 	@plot_pieces = PlotPiece.find(PlotPiece.pluck(:id).shuffle[0..4])
   end
