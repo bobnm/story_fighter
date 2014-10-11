013f3e9a21443c51b3290420f81d641aca973291
Bob <bobnmfrog@gmail.com>
2014-10-11 15:08:13 +0900

    install rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-StoryFighter::Application.config.secret_key_base = 'deee4afd7c4eef02e939a3da86fafe6c94b5a24bdaed6a7b08c628785b7f53876c1ccd2d0d88d47c093f4e61f017c95cc4475489894d139fd9248919eb1c2849'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+StoryFighter::Application.config.secret_key_base = secure_token
