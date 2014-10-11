1a190735fa487f11ba0e61a046eb0b02a4a1683a
Bob <bobnmfrog@gmail.com>
2014-10-11 15:03:30 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoryFighter::Application.config.secret_key_base = 'deee4afd7c4eef02e939a3da86fafe6c94b5a24bdaed6a7b08c628785b7f53876c1ccd2d0d88d47c093f4e61f017c95cc4475489894d139fd9248919eb1c2849'
