976c8c4d0f824ad3a6c398b1e68bc25cd8da2922
Bob <bobnmfrog@gmail.com>
2014-10-12 09:57:59 +0900

    create convention from random_plot

--- app/controllers/random_plot_controller.rb
+++ app/controllers/random_plot_controller.rb
@@ -5,7 +5,13 @@ class RandomPlotController < ApplicationController
 	end
 
 	def show
+		set_random_from_id_seed
 		@story_post = StoryPost.new
+		@convention = Convention.new
+	end
+
+private
+	def set_random_from_id_seed
 		@plot_pieces = PlotPiece.find(PlotPiece.pluck(:id).shuffle(random: Random.new(params[:id].to_i))[0..4])
 	end
 end
